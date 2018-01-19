8b9cd6f39b8f641bb533526104f4e8a25ee1299e
Zhenyi He <snowymohehe@gmail.com>
2018-01-19 00:49:33 -0500

    example for c++ client and unity server, based on request and reply pattern

---
+++ csharpServer/Assets/server.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class server : MonoBehaviour {
+
+    public bool Connected;
+    private NetMqPublisher _netMqPublisher;
+    private string _response;
+
+    private void Start()
+    {
+        _netMqPublisher = new NetMqPublisher(HandleMessage);
+        _netMqPublisher.Start();
+    }
+
+    private void Update()
+    {
+        var position = transform.position;
+        _response = "position:" + position.x + "," + position.y + "," + position.z +"\n";//$"{position.x} {position.y} {position.z}";
+        Connected = _netMqPublisher.Connected;
+    }
+
+    private string HandleMessage(string message)
+    {
+        // Not on main thread
+        return _response;
+    }
+
+    private void OnDestroy()
+    {
+        _netMqPublisher.Stop();
+    }
+}
